4a6277d55e908519216ccad6c9c353ea0c8c2567
Kris Reid <krisreid_06@hotmail.com>
2021-08-02 14:48:16 +0100

    Added code to UI Application main to allow Firebase code to work (and push notifications)

--- Shared/natrApp.swift
+++ Shared/natrApp.swift
@@ -6,12 +6,37 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct natrApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        FirebaseApp.configure()
+        Messaging.messaging().delegate = self
+        
+        return true
+    }
+
+    //Both functions required for push notifications
+    internal func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        completionHandler(UIBackgroundFetchResult.newData)
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken;
+    }
+
 }
