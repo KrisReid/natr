762402f35b2e138df1d7b2184bfd1733529a90ea
Kris Reid <krisreid_06@hotmail.com>
2021-08-02 14:25:15 +0100

    Initial Commit

---
+++ Shared/natrApp.swift
@@ -0,0 +1,17 @@
+//
+//  natrApp.swift
+//  Shared
+//
+//  Created by Kris Reid on 02/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct natrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
