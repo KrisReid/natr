6c6051c0fe81683a27f313c41edfc004f10f50e8
Kris Reid <krisreid_06@hotmail.com>
2021-09-10 21:06:33 +0100

    Store the private key into the keychain

--- Shared/Utilities/Crypto.swift
+++ Shared/Utilities/Crypto.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import CryptoKit
+import Security
 import FirebaseAuth
 import FirebaseFirestore
 import FirebaseFirestoreSwift
@@ -18,17 +19,6 @@ class Crypto: ObservableObject {
         let publicKeyBase64 = rawPublicKey.base64EncodedString()
         let percentEncodedPublicKey = publicKeyBase64.addingPercentEncoding(withAllowedCharacters: .alphanumerics)!
         return percentEncodedPublicKey
-        
-        //TEST THE CONVERSION BACK WORKS
-        
-//        do {
-//            let a = try importPublicKey(percentEncodedPublicKey)
-//            print("1111111111")
-//            print(a)
-//        } catch {
-//            print("The file could not be loaded")
-//        }
-        
     }
     
     
@@ -39,17 +29,54 @@ class Crypto: ObservableObject {
     }
     
     
-    
     func generateCryptoKeys() -> String {
         let privateKey = Curve25519.KeyAgreement.PrivateKey()
         let publicKey = privateKey.publicKey
-        
-        //Store the Private Key (On KeyChain)
-        
-        
-        //Store the Public Key in Firebase
+        storePrivateKey(privateKey)
         return exportPublicKey(publicKey)
+    }
+    
+    
+    func storePrivateKey(_ privateKey: Curve25519.KeyAgreement.PrivateKey) {
+        let query = [
+            kSecValueData: privateKey.rawRepresentation,
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: "private-key",
+            kSecAttrAccount: "natr"
+        ] as CFDictionary
         
+        let status = SecItemAdd(query, nil)
+        if status != errSecSuccess {
+            print("Error: \(status)")
+        } else {
+            print("Success!!!!!")
+        }
+    }
+    
+    
+    func retirevePrivateKey() -> Curve25519.KeyAgreement.PrivateKey {
+        let query = [
+            kSecAttrService: "private-key",
+            kSecAttrAccount: "natr",
+            kSecClass: kSecClassGenericPassword,
+            kSecReturnData: true
+        ] as CFDictionary
+        
+        var result: AnyObject?
+        SecItemCopyMatching(query, &result)
+        let rawPrivateKey = result as? Data
+        return try! Curve25519.KeyAgreement.PrivateKey(rawRepresentation: rawPrivateKey!)
+    }
+    
+    
+    func deletePrivateKey() {
+        let query = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: "private-key",
+            kSecAttrAccount: "natr"
+            ] as CFDictionary
+        
+        SecItemDelete(query)
     }
     
 }
